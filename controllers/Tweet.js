df3e428390b5b81f671723e04d870f1c681bf8a2
Bahbi Lee <superbahbi@users.noreply.github.com>
2020-02-03 12:21:01 -0800

    Tweet validation

--- controllers/Tweet.js
+++ controllers/Tweet.js
@@ -6,27 +6,53 @@ const fs = require('fs');
 exports.tweet = (req, res, next) => {
   console.log(req.body);
   console.log(req.file);
+  let tweet = new Tweet;
   const validationErrors = [];
-  if (validator.isEmpty(req.body.tweet)) validationErrors.push({ msg: 'Tweet cannot be blank.' });
+  if (validator.isEmpty(req.body.tweet) && isEmptyObject(req.file)) {
+    validationErrors.push({
+      msg: 'Tweet cannot be blank.'
+    });
+  }
+  if (!isEmptyObject(req.file)) {
+    switch (req.file.mimetype) {
+      case "image/gif":
+      case "image/png":
+      case "image/jpeg":
+        tweet.img.data = req.file.buffer.toString("base64");
+        tweet.img.contentType = req.file.mimetype;
+        break;
+      default:
+        validationErrors.push({
+          msg: 'Invalid file type.'
+        });
+        break;
+    }
+  }
   if (validationErrors.length) {
     req.flash('error', validationErrors);
     return res.redirect('/home');
   }
-
-  User.findOne({username: req.session.passport.user.username }, function (err, user) {
-    let tweet = new Tweet;
+  User.findOne({
+    username: req.session.passport.user.username
+  }, function(err, user) {
     tweet.username = req.session.passport.user.username;
     tweet.name = user.profile.name;
     tweet.timestamp = new Date();
-    tweet.content = req.body.tweet,
-    tweet.img.data = req.file.buffer.toString("base64");
-    tweet.img.contentType = req.file.mimetype;
+    tweet.content = req.body.tweet;
     tweet.save(function(err) {
       if (err) {
         req.flash('error', 'Unable to save tweet');
       }
-      res.redirect('/home');
     });
   });
-
+  res.redirect('/home');
 };
+
+function isEmptyObject(obj) {
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      return false;
+    }
+  }
+  return true;
+}
