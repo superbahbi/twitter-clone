5e0d7cb47d6329625707b5e93dc9cd793b8e7b09
Bahbi Lee <superbahbi@users.noreply.github.com>
2020-02-23 09:06:43 -0800

    Moved spinner to the middle

--- client/src/Routes/PrivateRoute.jsx
+++ client/src/Routes/PrivateRoute.jsx
@@ -1,7 +1,19 @@
 import React, { useContext } from "react";
+import styled from "styled-components";
 import { Route, Redirect } from "react-router-dom";
 import { authContext } from "../Contexts/AuthContext";
 import ClipLoader from "react-spinners/ClipLoader";
+const Spinner = styled.div`
+  position: absolute;
+  height: 100px;
+  width: 100px;
+  top: 50%;
+  left: 50%;
+  margin-left: -50px;
+  margin-top: -50px;
+  background-size: 100%;
+`;
+
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const { auth } = useContext(authContext);
   const { loading } = auth;
@@ -12,12 +24,14 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
         {...rest}
         render={() => {
           return (
-            <ClipLoader
-              size={150}
-              //size={"150px"} this also works
-              color={"#1DA1F2"}
-              loading={loading}
-            />
+            <Spinner>
+              <ClipLoader
+                size={150}
+                //size={"150px"} this also works
+                color={"#1DA1F2"}
+                loading={loading}
+              />
+            </Spinner>
           );
         }}
       />
