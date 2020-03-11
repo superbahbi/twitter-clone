19fb0c9de24ca39acd826cfc8a78ec62b0cba2e7
Bahbi Lee <superbahbi@users.noreply.github.com>
2020-03-10 20:30:16 -0700

    Update fetch.js

--- client/src/Helper/fetch.js
+++ client/src/Helper/fetch.js
@@ -1,8 +1,10 @@
 export async function fetchDB(param, method, signal) {
-  let api_url =
-    process.env.REACT_APP_API_URL || "https://yetanothertwitter.herokuapp.com";
   console.log(api_url);
-  const response = await fetch(`${api_url}/api${param}`, method, { signal });
+  const response = await fetch(
+    `https://yetanothertwitter.herokuapp.com/api${param}`,
+    method,
+    { signal }
+  );
   return {
     status: response.status,
     data: await response.json()
