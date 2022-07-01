fc2ec950da41e19b05251f54c0ca9c69239867f8
Bobby Lee <superbahbi@users.noreply.github.com>
2022-06-30 21:40:37 -0700

    added placeholder component

--- src/Views/Profile.jsx
+++ src/Views/Profile.jsx
@@ -7,36 +7,7 @@ import useTweet from "../Hooks/useTweet";
 import Feed from ".././Components/Feed";
 import Header from "../Components/Header";
 import ProfileBox from "../Components/ProfileBox";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
-
-const SkeletonContainer = styled.div`
-  display: flex;
-  margin-top: 16px;
-  width: 550px;
-  margin-top: 16px;
-  .avatar {
-    margin-left: 16px;
-    margin-right: 12px;
-    width: 50px;
-    height: 50px;
-  }
-  .name-group {
-    display: inline-flex;
-    .name {
-      width: 70px;
-      margin-right: 4px;
-    }
-  }
-  .content-group {
-    width: 425px;
-  }
-  .button-group {
-    display: flex;
-    width: 425px;
-    justify-content: space-between;
-  }
-`;
+import Placeholder from "../Components/Placeholder";
 function Profile() {
   let { profile } = useParams();
   const { getUserTweets, reset } = useTweet();
@@ -48,18 +19,14 @@ function Profile() {
   const [tweetCount, setTweetCount] = useState();
 
   useEffect(() => {
-    console.log(loading);
-
     const request = async () => {
       reset();
       setTimeout(() => {
         setLoading(false);
         getUserTweets(profile);
-      }, 2000);
+      }, 1000);
     };
     request();
-
-    console.log(loading);
   }, [reload]);
   // if (loading) {
   //   return <Button>LOADING</Button>;
@@ -77,90 +44,7 @@ function Profile() {
             username={authState.user.username}
           />
           {loading ? (
-            <>
-              <SkeletonTheme style={{ display: "inline-flex" }}>
-                <SkeletonContainer>
-                  <Skeleton className="avatar" circle={true} />
-                  <div>
-                    <div className="name-group">
-                      <Skeleton className="name" />
-                      <Skeleton className="name" />
-                      <Skeleton className="name" />
-                    </div>
-                    <div className="content-group">
-                      <Skeleton count={2} />
-                    </div>
-
-                    <div className="button-group">
-                      <Skeleton style={{ width: "20px" }} />
-                      <Skeleton style={{ width: "20px" }} />
-                      <Skeleton style={{ width: "20px" }} />
-                      <Skeleton style={{ width: "20px" }} />
-                    </div>
-                  </div>
-                </SkeletonContainer>
-                <SkeletonContainer>
-                  <Skeleton className="avatar" circle={true} />
-                  <div>
-                    <div className="name-group">
-                      <Skeleton className="name" />
-                      <Skeleton className="name" />
-                      <Skeleton className="name" />
-                    </div>
-                    <div className="content-group">
-                      <Skeleton count={2} />
-                    </div>
-
-                    <div className="button-group">
-                      <Skeleton style={{ width: "20px" }} />
-                      <Skeleton style={{ width: "20px" }} />
-                      <Skeleton style={{ width: "20px" }} />
-                      <Skeleton style={{ width: "20px" }} />
-                    </div>
-                  </div>
-                </SkeletonContainer>
-                <SkeletonContainer>
-                  <Skeleton className="avatar" circle={true} />
-                  <div>
-                    <div className="name-group">
-                      <Skeleton className="name" />
-                      <Skeleton className="name" />
-                      <Skeleton className="name" />
-                    </div>
-                    <div className="content-group">
-                      <Skeleton count={2} />
-                    </div>
-
-                    <div className="button-group">
-                      <Skeleton style={{ width: "20px" }} />
-                      <Skeleton style={{ width: "20px" }} />
-                      <Skeleton style={{ width: "20px" }} />
-                      <Skeleton style={{ width: "20px" }} />
-                    </div>
-                  </div>
-                </SkeletonContainer>
-                <SkeletonContainer>
-                  <Skeleton className="avatar" circle={true} />
-                  <div>
-                    <div className="name-group">
-                      <Skeleton className="name" />
-                      <Skeleton className="name" />
-                      <Skeleton className="name" />
-                    </div>
-                    <div className="content-group">
-                      <Skeleton count={2} />
-                    </div>
-
-                    <div className="button-group">
-                      <Skeleton style={{ width: "20px" }} />
-                      <Skeleton style={{ width: "20px" }} />
-                      <Skeleton style={{ width: "20px" }} />
-                      <Skeleton style={{ width: "20px" }} />
-                    </div>
-                  </div>
-                </SkeletonContainer>
-              </SkeletonTheme>
-            </>
+            <Placeholder />
           ) : (
             <Feed
               tweets={tweetState && tweetState.tweets}
