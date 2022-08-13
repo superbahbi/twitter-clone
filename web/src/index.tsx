99ea4944eb29bcd279952edef1ac784387d7d07e
Bobby Lee <superbahbi@gmail.com>
2022-08-12 19:45:42 -0700

    added theme provider from styled component

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,12 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import React from "react";
-
 import ReactDOM from "react-dom";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createGlobalStyle } from "styled-components";
-
+import theme from "./theme";
 const AppRoot = createGlobalStyle`
   body {
     font-family: Chirp !important;
@@ -14,10 +12,10 @@ const AppRoot = createGlobalStyle`
   }
 `;
 ReactDOM.render(
-  <>
+  <ThemeProvider theme={theme}>
     <AppRoot />
     <App />
-  </>,
+  </ThemeProvider>,
   document.getElementById("root")
 );
 
---
+++ web/src/theme.ts
@@ -0,0 +1,27 @@
+export default {
+  main: "#1da1f2",
+  color: {
+    main: "#1da1f2",
+    white: "#ffffff",
+    text: "#0f1419",
+    lightText: "#536471",
+    border: "#eff3f4",
+    error: "#ff0000",
+    hover: "#1a91da",
+  },
+  fontSize: {
+    small: "12px",
+    medium: "15px",
+    large: "17px",
+  },
+  padding: {
+    small: "16px",
+    medium: "30px",
+    large: "9999px",
+  },
+  fontWeight: {
+    light: "300",
+    regular: "600",
+    bold: "900",
+  },
+};
