782449985a996f3abcf8207288773d4f4ab78d9c
Bobby Lee <superbahbi@gmail.com>
2022-08-12 19:43:34 -0700

    remove theme provider from react bootstrap

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import ThemeProvider from "react-bootstrap/ThemeProvider";
 import { BrowserRouter } from "react-router-dom";
 import { Provider as AuthProvider } from "./Contexts/AuthContext";
 import { Provider as TweetProvider } from "./Contexts/TweetContext";
@@ -10,23 +9,18 @@ import CustomRoutes from "./CustomRoutes";
 const queryClient = new QueryClient();
 function App() {
   return (
-    <ThemeProvider
-      breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
-      minBreakpoint="xxs"
-    >
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <UserProvider>
-            <TweetProvider>
-              <BrowserRouter>
-                <CustomRoutes />
-                <ReactQueryDevtools initialIsOpen={true} />
-              </BrowserRouter>
-            </TweetProvider>
-          </UserProvider>
-        </AuthProvider>
-      </QueryClientProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <UserProvider>
+          <TweetProvider>
+            <BrowserRouter>
+              <CustomRoutes />
+              <ReactQueryDevtools initialIsOpen={true} />
+            </BrowserRouter>
+          </TweetProvider>
+        </UserProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
 
